cd0a25d8910836aec0cc6380a0904f04a8df4a72
Juliana Ferreira Ribeiro <julianaferreiraribeiro04@gmail.com>
2023-09-21 23:00:43 -0300

    feat: user entity created

---
+++ src/entities/users.entity.ts
@@ -0,0 +1,31 @@
+import { Column, CreateDateColumn, DeleteDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+@Entity("users")
+export class User{
+
+    @PrimaryGeneratedColumn("increment")
+    id: number;
+
+    @Column({type: "varchar", length: 45})
+    name: string;
+
+    @Column({type: 'varchar', length: 45, unique: true})
+    email: string;
+
+    @Column({type: "varchar", length: 120})
+    password: string;
+
+    @Column({default: false})
+    admin: boolean;
+
+    @CreateDateColumn({type: 'date'})
+    createdAt: string;
+
+    @UpdateDateColumn({type: 'date'})
+    updatedAt: string;
+
+    @DeleteDateColumn({type: 'date'})
+    deletedAt: string;
+
+
+}
