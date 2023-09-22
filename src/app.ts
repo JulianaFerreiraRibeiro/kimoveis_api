6396379299dabdd66d868c850bb8d0c86a9f7c17
Juliana Ferreira Ribeiro <julianaferreiraribeiro04@gmail.com>
2023-09-22 18:29:27 -0300

    feat: created login router

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express, { json } from "express";
 import { UsersRouter } from "./routers/users.router";
+import { SessionRouter } from "./routers/session.router";
 
 export const app = express();
 app.use(json())
 
 app.use("/users", UsersRouter)
+app.use("/login", SessionRouter)
 
 export default app 
