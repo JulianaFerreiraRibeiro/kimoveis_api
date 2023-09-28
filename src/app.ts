c6bcc03877aec3fdf953e2cb2d83745d23cb1879
Juliana Ferreira Ribeiro <julianaferreiraribeiro04@gmail.com>
2023-09-27 23:14:56 -0300

    feat: realEstates router and export to app

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import { UsersRouter } from "./routers/users.router";
 import { SessionRouter } from "./routers/session.router";
 import { SchedulesRouter } from "./routers/schedules.router";
 import { CategoriesRouter } from "./routers/categories.router";
+import { RealEstatesRouter } from "./routers/realEstates.router";
 
 export const app = express();
 app.use(json())
@@ -11,5 +12,6 @@ app.use("/users", UsersRouter)
 app.use("/login", SessionRouter)
 app.use("/schedules", SchedulesRouter)
 app.use("/categories", CategoriesRouter)
+app.use("/realEstates", RealEstatesRouter)
 
 export default app 
