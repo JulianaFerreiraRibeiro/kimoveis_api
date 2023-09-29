799b5851d925aad5e2c98b454a715a80b8d36eaf
Juliana Ferreira Ribeiro <julianaferreiraribeiro04@gmail.com>
2023-09-29 19:04:59 -0300

    feat: realEstates interface typing file

--- src/repositories.ts
+++ src/repositories.ts
@@ -1,12 +1,13 @@
 import { AppDataSource } from "./data-source";
 import { Address, Category, RealEstate, Schedule, User } from "./entities";
+import { RepoRealEstate } from "./interfaces/realEstates.interface";
 import { RepoSchedule } from "./interfaces/schedules.interface";
 import { RepoUser } from "./interfaces/users.interface";
 
 export const usersRepository: RepoUser = AppDataSource.getRepository(User)
 export const categoriesRepository = AppDataSource.getRepository(Category)
 export const schedulesRepository: RepoSchedule = AppDataSource.getRepository(Schedule)
-export const realEstatesRepository = AppDataSource.getRepository(RealEstate)
+export const realEstatesRepository: RepoRealEstate = AppDataSource.getRepository(RealEstate)
 export const addressesRepository = AppDataSource.getRepository(Address)
 
 
