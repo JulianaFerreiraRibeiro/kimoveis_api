9e4bfac9a9dc12c4d7c4e933f18c6e2e80bb17bd
Juliana Ferreira Ribeiro <julianaferreiraribeiro04@gmail.com>
2023-10-04 23:04:13 -0300

    feat: verify req body middleware

--- src/routers/users.router.ts
+++ src/routers/users.router.ts
@@ -1,12 +1,14 @@
 import { Router } from "express";
 import { createUserController, deleteUserController, readUserController, updateUserController } from "../controllers/users.controller";
 import handleToken from "../middlewares/verifyToken/handleToken.middleware";
+import handleBody from "../middlewares/verifyBody/handleBody.middleware";
+import { usersCreateSchema, usersUpdateSchema } from "../schemas/users.schema";
 
 export const UsersRouter = Router()
 
-UsersRouter.post("/", createUserController)
+UsersRouter.post("/", handleBody(usersCreateSchema), createUserController)
 UsersRouter.get("/", handleToken, readUserController)
-UsersRouter.patch("/:userId", handleToken, updateUserController)
+UsersRouter.patch("/:userId", handleBody(usersUpdateSchema), handleToken, updateUserController)
 UsersRouter.delete("/:userId", handleToken, deleteUserController)
 
 
