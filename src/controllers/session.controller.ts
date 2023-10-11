ee5302b4caa04b02664b257c5f04902adaa2f8b7
Juliana Ferreira Ribeiro <julianaferreiraribeiro04@gmail.com>
2023-10-10 23:39:31 -0300

    feat: user login logic

--- src/controllers/session.controller.ts
+++ src/controllers/session.controller.ts
@@ -1,6 +1,10 @@
 import { Request, Response } from "express";
+import { ReturnSession } from "../interfaces/session.interface";
+import { sessionUserService } from "../services/session.service";
 
-export const sessionUserController = (req: Request, res: Response): Response => {
+export const sessionUserController = async (req: Request, res: Response): Promise<Response> => {
 
-    return res.status(200).json()
+    const token: ReturnSession = await sessionUserService(req, res) 
+
+    return res.status(200).json(token)
 }
