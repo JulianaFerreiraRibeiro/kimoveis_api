80293cb5be4ae3fd713dfa9319502d00cb574412
Juliana Ferreira Ribeiro <julianaferreiraribeiro04@gmail.com>
2023-10-18 18:22:33 -0300

    feat: read category realEstate

--- src/services/categories.service.ts
+++ src/services/categories.service.ts
@@ -1,3 +1,4 @@
+import AppError from "../../error";
 import { CreateCategory } from "../interfaces/categories.interface";
 import { categoriesRepository } from "../repositories";
 
@@ -9,4 +10,22 @@ export const createCategoryService = async (payload: CreateCategory) => {
 export const readCategoriesService = async () => {
 
     return await categoriesRepository.find()
+}
+
+export const readCategoriesRealEstateService = async (id: number) => {
+
+    const foundCategory = await categoriesRepository.findOneBy({id: id})
+
+    if (!foundCategory) throw new AppError("Category not found", 404)
+
+    const foundCategoryRealEstate = await categoriesRepository.findOne({
+        where: {
+            id: id
+        },
+        relations: {
+            realEstate: true
+        }
+    })
+
+    return foundCategoryRealEstate
 }
